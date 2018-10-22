2946910515822717176cde865987fcf691d95195
Nazar Rohozhuk <nazarrrka@gmail.co>
2018-10-23 00:42:27 +0300

    init

---
+++ precache-manifest.5e866f1c0b6787a332d8316086fb4c69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8e5fb6f576b28beac0f1",
+    "url": "/static/js/main.8e5fb6f5.chunk.js"
+  },
+  {
+    "revision": "c328ad9e0f3afaa51371",
+    "url": "/static/js/1.c328ad9e.chunk.js"
+  },
+  {
+    "revision": "8e5fb6f576b28beac0f1",
+    "url": "/static/css/main.ec1ccf25.chunk.css"
+  },
+  {
+    "revision": "c328ad9e0f3afaa51371",
+    "url": "/static/css/1.70ba75e3.chunk.css"
+  },
+  {
+    "revision": "339a9500701b83e3974f29117cfd0fdb",
+    "url": "/index.html"
+  }
+];
